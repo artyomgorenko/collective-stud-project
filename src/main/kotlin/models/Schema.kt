113bc1122347bc3a3cebff1d64203008cbf33bfb
artyomgorenko <artyomgorenko@gmail.com>
2018-10-20 10:37:29 +0300

    #2 добавлен класс для Схемы

--- src/main/kotlin/models/Schema.kt
+++ src/main/kotlin/models/Schema.kt
@@ -1,4 +1,12 @@
 package models
 
 class Schema {
+    // atributes
+    var name: String = ""
+    var fulltest_engine: String = ""
+    var description: String = ""
+
+    // child tags
+    var domains: List<Domain>? = null
+    var tables: List<Table>? = null
 }
--- src/main/kotlin/models/Table.kt
+++ src/main/kotlin/models/Table.kt
@@ -1,13 +1,15 @@
 package models
 
 class Table {
+    // atributes
     var name: String = ""
     var description: String = ""
     var ht_table_flags : Boolean = false
     var accessLevel : Int = 0
+    var properties: List<String>? = null
 
+    // child tags
     var fields: List<Field>? = null
     var constraints: List<Constraint>? = null
     var indexes: List<Index>? = null
-    var properties: List<String>? = null
 }
